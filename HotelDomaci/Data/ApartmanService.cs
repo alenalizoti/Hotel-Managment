b1dda4743fe51e197d6232e76a801773e1bf2602
alenalizoti <alenalizoti13@gmail.com>
2025-05-12 18:23:18 +0200

    Povezivanje sa MongoDB bazom, kreiranje servisa i modela

---
+++ HotelDomaci/Data/ApartmanService.cs
@@ -0,0 +1,50 @@
+﻿using HotelDomaci.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace HotelDomaci.Data
+{
+    public class ApartmanService
+    {
+        private readonly IMongoCollection<Apartman> _apartmani;
+
+        public ApartmanService(IOptions<MongoDbSettings> mongoSettings)
+        {
+            var client = new MongoClient(mongoSettings.Value.ConnectionString);
+            var database = client.GetDatabase(mongoSettings.Value.DatabaseName);
+            _apartmani = database.GetCollection<Apartman>(mongoSettings.Value.ApartmaniCollection);
+        }
+
+        public async Task<List<Apartman>> GetAsync()
+        {
+            var result = await _apartmani.FindAsync(ap => true);
+            return await result.ToListAsync();
+        }
+        public async Task<Apartman?> GetAsync(string id)
+        {
+            var result = await _apartmani.FindAsync(ap => ap.Id == id);
+            return await result.FirstOrDefaultAsync();
+        }
+        public async Task CreateAsync(Apartman ap)
+        {
+            await _apartmani.InsertOneAsync(ap);
+        }
+
+        public async Task UpdateAsync(string id, Apartman ap)
+        {
+            await _apartmani.ReplaceOneAsync(a => a.Id == id, ap);
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            await _apartmani.DeleteOneAsync(a => a.Id == id);
+        }
+
+    }
+    public class MongoDbSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string ApartmaniCollection { get; set; }
+    }
+}
---
+++ HotelDomaci/Models/Apartman.cs
@@ -0,0 +1,46 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace HotelDomaci.Models
+{
+    public class Apartman
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("NazivApartmana")]
+        public string NazivApartmana { get; set; }
+
+        [BsonElement("Drzava")]
+        public string Drzava { get; set; }
+
+        [BsonElement("Grad")]
+        public string Grad { get; set; }
+
+        [BsonElement("UdaljenostOdCentra")]
+        public double UdaljenostOdCentra { get; set; }
+
+        [BsonElement("CenaPoNocenju")]
+        public decimal CenaPoNocenju { get; set; }
+
+        [BsonElement("OpisApartmana")]
+        public string OpisApartmana { get; set; }
+
+        [BsonElement("ServisneUsluge")]
+        public ServisneUsluge ServisneUsluge { get; set; }
+
+        [BsonElement("BrojMesta")]
+        public int BrojMesta { get; set; }
+
+        [BsonElement("Slike")]
+        public List<string> Slike { get; set; } = new List<string>();
+    }
+    public class ServisneUsluge
+    {
+        public bool Klima { get; set; }
+        public bool Wifi { get; set; }
+        public bool Frizider { get; set; }
+        public bool Sef { get; set; }
+    }
+}
